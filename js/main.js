3893858f1ce938e206396a56b6db52945895e203
melmorales22 <148358788+melmorales22@users.noreply.github.com>
2023-10-23 20:16:52 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var password;
+var pass1 = "Rommel";
+password = prompt("Please enter your password to view this page!");
+    if (password==pass1)
+    {
+        alert("Password is Correct! Click OK to enter!");
+    }
+    else
+    {
+        window.location = "practice_js2.html"
+    }
---
+++ js/main2.js
@@ -0,0 +1,14 @@
+var yourNumber = window.prompt ("Enter a Number");
+
+    if (yourNumber > 10)
+    {
+        alert("Your Number is greater than 10!")
+    }
+    else if (yourNumber == 10)
+    {
+        alert("Your Number is exactly 10!")
+    }
+    else
+    {
+        alert("Your Number is less than 10!")
+    }
